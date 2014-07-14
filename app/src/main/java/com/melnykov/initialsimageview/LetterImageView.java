706bc32256736116c50cb6f6ec8f4d866f174cdc
makovkastar <makovkastar@gmail.com>
2014-07-14 14:40:12 +0300

    Initial commit

---
+++ app/src/main/java/com/melnykov/initialsimageview/LetterImageView.java
@@ -0,0 +1,78 @@
+package com.melnykov.initialsimageview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class LetterImageView extends ImageView {
+
+    private char mLetter;
+    private Paint mTextPaint;
+    private int mTextColor = Color.WHITE;
+    private int mBackgroundColor;
+
+    public LetterImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        mTextPaint = new Paint();
+        mTextPaint.setColor(mTextColor);
+        mBackgroundColor = randomColor();
+    }
+
+    public char getLetter() {
+        return mLetter;
+    }
+
+    public void setLetter(char letter) {
+        mLetter = letter;
+        invalidate();
+    }
+
+    public int getTextColor() {
+        return mTextColor;
+    }
+
+    public void setTextColor(int textColor) {
+        mTextColor = textColor;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (getDrawable() == null) {
+            // Set a text font size based on the height of the view
+            mTextPaint.setTextSize(canvas.getHeight() - getTextPadding() * 2);
+            // Draw a background color
+            canvas.drawColor(mBackgroundColor);
+            // Measure a text
+            Rect textBounds = new Rect();
+            mTextPaint.getTextBounds(String.valueOf(mLetter), 0, 1, textBounds);
+            float textWidth = mTextPaint.measureText(String.valueOf(mLetter));
+            float textHeight = textBounds.height();
+            // Draw the text
+            canvas.drawText(String.valueOf(mLetter), canvas.getWidth() / 2 - textWidth / 2,
+                    canvas.getHeight() / 2 + textHeight / 2, mTextPaint);
+        }
+    }
+
+    private float getTextPadding() {
+        // Set a default padding to 8dp
+        return 8 * getResources().getDisplayMetrics().density;
+    }
+
+    private int randomColor() {
+        Random random = new Random();
+        String[] colorsArr = getResources().getStringArray(R.array.colors);
+        return Color.parseColor(colorsArr[random.nextInt(colorsArr.length)]);
+    }
+}
---
+++ app/src/main/java/com/melnykov/initialsimageview/MainActivity.java
@@ -0,0 +1,46 @@
+package com.melnykov.initialsimageview;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ListAdapter listAdapter = new SampleListAdapter(this, Arrays.asList(getResources().getStringArray(R.array.names)));
+        setListAdapter(listAdapter);
+    }
+
+    private static class SampleListAdapter extends ArrayAdapter<String> {
+
+        public SampleListAdapter(Context context, List<String> objects) {
+            super(context, R.layout.list_item, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            String name = getItem(position);
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+            }
+
+            LetterImageView letterImageView = (LetterImageView) convertView.findViewById(R.id.iv_avatar);
+            TextView textView = (TextView) convertView.findViewById(R.id.tv_name);
+            letterImageView.setLetter(name.charAt(0));
+            textView.setText(name);
+
+            return convertView;
+        }
+    }
+}
